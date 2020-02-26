7d0622e5fb6f5caa9f752ea4cedb4b420178a1dc
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-02-25 20:19:07 -0500

    Update V-82139.rb

--- controls/V-82139.rb
+++ controls/V-82139.rb
@@ -46,7 +46,7 @@ Administrative Templates >> Windows Components >> Microsoft Edge >> \"Prevent
 certificate error overrides\" to \"Enabled\"."
   describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\MicrosoftEdge\\Internet') do
     it {should have_property 'PreventCertErrorOverrides' }
-    its('PreventCertErrorOverrides') {should cmp == 0x00000001}
+    its('PreventCertErrorOverrides') { should cmp == 0x00000001 }
 end
   
 end
