cc584ec78fa5e323589f8c334338663f8a9c1797
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-02-25 20:19:30 -0500

    Update V-82137.rb

--- controls/V-82137.rb
+++ controls/V-82137.rb
@@ -44,7 +44,7 @@ Copy the OneDrive.admx and .adml files to the \\Windows\\PolicyDefinitions and
 \\Windows\\PolicyDefinitions\\en-US directories respectively."
   describe registry_key('HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\OneDrive') do
     it { should have_property 'DisablePersonalSync' }
-    its('DisablePersonalSync') {should cmp == 0x00000001}
+    its('DisablePersonalSync') { should cmp == 0x00000001 }
 end
 
 end
