670d1be3f97708132bb8cdc9853aca2949f29aca
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-02-26 06:02:29 -0800

    Update V-74415.rb

--- controls/V-74415.rb
+++ controls/V-74415.rb
@@ -41,8 +41,9 @@ Value: 0x00000000 (0)"
   tag fix: "Configure the policy value for Computer Configuration >>
 Administrative Templates >> Windows Components >> Microsoft Edge >> \"Allow
 clearing browsing data on exit\" to \"Disabled\"."
-  describe registry_key('\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Privacy') do
-    its('ClearBrowsingHistoryOnExit') {should eq 0x00000000}
+  describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\MicrosoftEdge\\Privacy') do
+    it { should have_property 'ClearBrowsingHistoryOnExit' }
+    its('ClearBrowsingHistoryOnExit') { should cmp == 0x00000000 }
 end
   
 end
