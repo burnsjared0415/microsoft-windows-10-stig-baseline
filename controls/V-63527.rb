5cf85abdc070ef3f3b9a655730ca3c2f53331119
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-02 15:11:50 -0500

    Updated Control and Moved from Draft

--- controls/V-63527.rb
+++ controls/V-63527.rb
@@ -42,5 +42,9 @@ Configure the policy value for Computer Configuration >> Administrative
 Templates >> Windows Components >> Event Log Service >> System >> \"Specify the
 maximum log file size (KB)\" to \"Enabled\" with a \"Maximum Log Size (KB)\" of
 \"32768\" or greater."
+   describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\EventLog\\System') do
+    it { should have_property 'MaxSize' }
+    its('MaxSize') { should cmp >= 32768 }
+  end
+  
 end
-
