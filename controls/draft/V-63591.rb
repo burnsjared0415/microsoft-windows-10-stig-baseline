95debcd336d72e8d6166bfebcb1fde3e93a06b3f
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-02 16:39:15 -0500

    Updated Control and Moved out of Draft

--- controls/draft/V-63591.rb
+++ controls/draft/V-63591.rb
@@ -44,5 +44,10 @@ by contacts, and to hotspots offering paid services\" to \"Disabled\".
 v1507 LTSB does not include this group policy setting.  It may be configured
 through other means such as using group policy from a later version of Windows
 10 or a registry update."
+   describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\WcmSvc\\wifinetworkmanager\\config') do
+    it { should have_property 'AutoConnectAllowedOEM' }
+   its('AutoConnectAllowedOEM') { should cmp == 0 }
+  end
+  
 end
 
