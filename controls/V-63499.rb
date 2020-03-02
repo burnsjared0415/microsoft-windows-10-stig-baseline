b81959a80400e25e02290669526a2479b329f454
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-02 14:30:58 -0500

    Updated Control and Moved from Draft

--- controls/V-63499.rb
+++ controls/V-63499.rb
@@ -47,5 +47,14 @@ System >> Other System Events - Success"
 Settings >> Security Settings >> Advanced Audit Policy Configuration >> System
 Audit Policies >> System >> \"Audit Other System Events\" with \"Success\"
 selected."
+  describe.one do
+    describe audit_policy do
+      its('Other System Events') { should eq 'Success' }
+    end
+    describe audit_policy do
+      its('Other System Events') { should eq 'Success and Failure' }
+    end
+  end
+  
 end
 
