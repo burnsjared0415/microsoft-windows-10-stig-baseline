0b6baebe06bbb03cbce8f7ac5df209363f6acf3b
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-02 08:52:45 -0500

    Updated Control

--- controls/V-63405.rb
+++ controls/V-63405.rb
@@ -41,5 +41,14 @@ Settings >> Security Settings >> Account Policies >> Account Lockout Policy >>
 
 A value of \"0\" is also acceptable, requiring an administrator to unlock the
 account."
+  describe.one do
+    describe security_policy do
+      its('LockoutDuration') { should be >= 15 }
+    end
+    describe security_policy do
+      its('LockoutDuration') { should cmp == 0 }
+    end
+  end
+  
 end
 
