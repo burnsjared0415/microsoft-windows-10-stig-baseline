68a360985884a30449e9bd9bf985092bd431d4d2
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-02 13:38:01 -0500

    Updated Control

--- controls/V-63435.rb
+++ controls/V-63435.rb
@@ -47,5 +47,14 @@ Account Logon >> Credential Validation - Success"
 Settings >> Security Settings >> Advanced Audit Policy Configuration >> System
 Audit Policies >> Account Logon >> \"Audit Credential Validation\" with
 \"Success\" selected."
+  describe.one do
+    describe audit_policy do
+      its('Credential Validation') { should eq 'Success' }
+    end
+    describe audit_policy do
+      its('Credential Validation') { should eq 'Success and Failure' }
+    end
+  end
+  
 end
 
