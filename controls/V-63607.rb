c1b4c35ae6443fec8f1e703f5aa6e5bb9458efee
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-03 08:13:20 -0500

    Updated Control and Moved out of Draft

--- controls/V-63607.rb
+++ controls/V-63607.rb
@@ -55,5 +55,12 @@ the policy value for Computer Configuration >> Administrative Templates >>
 System >> Early Launch Antimalware >> \"Boot-Start Driver Initialization
 Policy\" to \"Not Configured\" or \"Enabled\" with any option other than
 \"All\" selected."
+  describe registry_key('HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Policies\\EarlyLaunch') do
+    it { should have_property 'DriverLoadPolicy' }
+    its('DriverLoadPolicy') { should cmp == 1 }
+    its('DriverLoadPolicy') { should cmp == 3 }
+    its('DriverLoadPolicy') { should cmp == 8 }
+  end
+  
 end
 
