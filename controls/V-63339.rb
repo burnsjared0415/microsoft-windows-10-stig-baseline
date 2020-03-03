ef9921e9ee4f96f6c55ad3b41f1e76b1f96f0020
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-03 12:42:04 -0500

    Update V-63339.rb

--- controls/V-63339.rb
+++ controls/V-63339.rb
@@ -37,10 +37,9 @@ Value: 0"
 Administrative Templates >> Windows Components >> Windows Remote Management
 (WinRM) >> WinRM Client >> \"Allow unencrypted traffic\" to \"Disabled\"."
 
-describe registry_key('HKEY_LOCAL_MACHIN\\SOFTWARE\\Policies\\Microsoft\\Windows\\WinRM\\Client') do
+  describe registry_key('HKEY_LOCAL_MACHIN\\SOFTWARE\\Policies\\Microsoft\\Windows\\WinRM\\Client') do
   it { should have_property 'AllowUnencryptedTraffic' }
   its('AllowUnencryptedTraffic') { should cmp == 0 }
-end
-
+  end
 end
 
