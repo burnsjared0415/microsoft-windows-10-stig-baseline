5d8c1fa6c9e2827d4bc84b315a7e5b787c68358a
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-03 12:57:06 -0500

    Updated Control and Moved out of Draft

--- controls/V-94861.rb
+++ controls/V-94861.rb
@@ -37,5 +37,9 @@ Value: 0x00000006 (6) or greater"
 Administrative Templates >> Windows Components >> BitLocker Drive Encryption >>
 Operating System Drives \"Configure minimum PIN length for startup\" to
 \"Enabled\" with \"Minimum characters:\" set to \"6\" or greater."
+  describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\Microsoft\Windows\CurrentVersion\Bitlocker') do
+    it { should have_property 'MinimumPIN' }
+    its('MinimumPIN') { should cmp >= 6 }
+  end
 end
 
