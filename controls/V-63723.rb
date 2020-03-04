42e582a4f27737a90daa17ddac89b1fdd9cdbd38
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-04 09:21:31 -0500

    Updated Control and Moved out of Draft

--- controls/V-63723.rb
+++ controls/V-63723.rb
@@ -37,5 +37,9 @@ Value: 1"
 Settings >> Security Settings >> Local Policies >> Security Options >>
 \"Microsoft network server: Digitally sign communications (if Client agrees)\"
 to \"Enabled\"."
+  describe registry_key('HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\LanManServer\\Parameters') do
+    it { should have_property 'EnableSecuritySignature' }
+    its('EnableSecuritySignature') { should cmp == 1 }
+  end
 end
 
