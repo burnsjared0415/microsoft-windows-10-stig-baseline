dab18d691cb30d3b4bba00fe725940593b977633
burnsjared0415 <61466028+burnsjared0415@users.noreply.github.com>
2020-03-04 07:57:18 -0500

    Updated Control and Moved out of Draft

--- controls/V-63657.rb
+++ controls/V-63657.rb
@@ -35,5 +35,9 @@ Value: 1"
   tag fix: "Configure the policy value for Computer Configuration >>
 Administrative Templates >> System >> Remote Procedure Call >> \"Restrict
 Unauthenticated RPC clients\" to \"Enabled\" and \"Authenticated\"."
+  describe registry_key('HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\Rpc') do
+    it { should have_property 'RestrictRemoteClients' }
+    its('RestrictRemoteClients') { should cmp == 1 }
+  end
 end
 
